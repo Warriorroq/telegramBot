7456752dbcfc565b8816aad136143b9060fd45a4
Arthur <Warriorroq@gmail.com>
2022-04-29 14:00:57 +0200

    added random num func

--- myBot.py
+++ myBot.py
@@ -1,13 +1,21 @@
 import logging
+import re
 from telegram import *
 from telegram.ext import *
 from requests import *
+from random import *
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 updater = Updater("5347249231:AAEJAR3s1oTRrbz1ex6-epcK_Ds95Npq38A", use_context=True)
 dp = updater.dispatcher
+
 this_person_not_exist = "https://thispersondoesnotexist.com/image"
+random_numbers_texts = [
+    "Sammy has {} balloons.",
+    "You have {} girlfriends.",
+    "{} your unlucky number",
+    ]
 
 
 def bot_start():
@@ -19,9 +27,27 @@ def bot_start():
 
 
 def handle_message(update, context):
-    if "Give random person" in update.message.text:
+    msg_text = update.message.text
+    if "Give random person" in msg_text:
         send_non_existing_person(update, context)
+    if "Random num" in msg_text:
+        send_random_integer(update, context)
 
+
+def send_random_integer(update, context):
+    nums = re.findall(r'\d+', update.message.text)
+    item = random_numbers_texts[randint(0, len(random_numbers_texts) - 1)]
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=item.format(get_randint_from_last_array_elements(nums)),
+    )
+
+
+def get_randint_from_last_array_elements(nums):
+    if len(nums) >= 3:
+        return randint(int(nums[-1]), int(nums[-2]))
+    else:
+        return randint(0, 10)
 
 def send_non_existing_person(update, context):
     image = get(this_person_not_exist).content
